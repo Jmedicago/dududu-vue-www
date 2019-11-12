b487df72af856fe31c927b9f19851f3795300e36
wenjf <wenjf1996@aliyun.com>
2019-11-12 23:59:48 +0800

    【新增】项目初始化

---
+++ src/page/home/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Header></Header>
+    <div class="btn-group" role="group" aria-label="...">
+      <button type="button" class="btn btn-default">Left</button>
+      <button type="button" class="btn btn-default">Middle</button>
+      <button type="button" class="btn btn-default">Right</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  import 'bootstrap/dist/css/bootstrap.min.css'
+  import 'bootstrap/dist/js/bootstrap.min.js'
+  import Header from "../../components/header";
+
+  export default {
+    name: "index",
+    components: {Header}
+  }
+</script>
+
+<style scoped>
+
+</style>
